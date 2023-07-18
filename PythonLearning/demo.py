b70b4ce8151cc785d22dafa222cca54390297c42
krishna <saran.oct1@gmail.com>
2023-07-18 08:28:31 +0530

    democommit

---
+++ PythonLearning/demo.py
@@ -0,0 +1,33 @@
+'School mark sheet'
+# Mark = int(input("Enter the mark : "))
+# if Mark < 25:
+#     print("Grade F")
+# elif Mark >= 25 and Mark < 45:
+#     print("Grade E")
+# elif Mark >= 45 and Mark < 50:
+#     print("Grade D")
+# elif Mark >= 50 and Mark < 60:
+#     print("Grade C")
+# elif Mark >= 60 and Mark < 80:
+#     print("Grade B")
+# elif Mark >= 80:
+#     print("Grade A")
+'Age comparison older'
+# Age_1 = int(input("Enter the age :"))
+# Age_2 = int(input("Enter the age :"))
+# Age_3 = int(input("Enter the age :"))
+# if Age_1 > Age_2 and Age_1 > Age_3:
+#     print(f"{Age_1} is greater than {Age_2} and {Age_3}")
+# elif Age_2 > Age_1 and Age_2 > Age_3:
+#     print(f"{Age_2} is greater than {Age_1} and {Age_3}")
+# elif Age_3 > Age_1 and Age_3 > Age_2:
+#     print(f"{Age_3} is greater than {Age_1} and {Age_2}")
+'Student exam attendance criteria'
+# Name_1 = input("Enter student name : ")
+# Class_held = int(input("Total no of class held : "))
+# Class_attended = int(input("Total no of class attended : "))
+# a = (Class_attended / Class_held) * 100
+# if a >= 75:
+#     print(f'{Name_1} is allowed to take exam as his attendance percentage is {round(a,2)}%')
+# else:
+#     print(f'{Name_1} is not allowed to take exam as his attendance percentage is {round(a, 2)}%')
