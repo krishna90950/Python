36774ec31276fd73454ef1b38bec674b1ebee5e1
krishna <saran.oct1@gmail.com>
2023-07-18 08:54:32 +0530

    List

---
+++ PythonLearning/List.py
@@ -0,0 +1,86 @@
+'reverse a list'
+
+# Lis_1 = [1, 'tom', -3, 8.7, (7 + 8j)]
+# print(Lis_1[::-1])
+# Lis_1.reverse()
+# print(Lis_1)
+
+'List sample question'
+# Name = ['yahoo', 'google']
+# print(Name[1][3])
+'List sample question 2'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name[:2] =['notepad', 'google forms']
+# print(name)
+
+'program to add an element in list'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name.append('whatsapp')
+# print(name)
+'program to add an element in list unpacking'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name.extend('whatsapp')
+# print(name)
+'program to add an element in particular index of list'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name.insert(2, 'whatsapp')
+# print(name)
+'program to remove last element from list'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name_1 = name[:-1]
+# print(name_1)
+'program to remove particular element from list index'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# del name[2]
+# print(name)
+'program to remove particular element from list'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name.remove('Amazon')
+# print(name)
+'program to remove more than one element from list'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# print(name[0:3:2])
+'program to copy a list'
+# import copy
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name_u = copy.deepcopy(name)
+# print(name, name_u)
+'program to convert string into a list'
+# v = list('string')
+# print(v)
+'program to sort list in based on ascii'
+
+# b = [1, 3, 7, 2, 4, 9, 6, 7]
+# b.sort()
+# print(b)
+# c = ['H', 'e', 'F', 't', 'S', 'w', 'C', 'Z', 'o']
+# c.sort()
+# print(c)
+'program to access element using index'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# print(name[3])
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name_2 = (name.index('Amazon'))
+# print(name_2)
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat']
+# name_2 = (name.index('Amazon'))
+# print(name_2)
+'program to count element using index'
+# name = ['google', 'instagram', 'facebook', 'Amazon', 'snapchat', 'google']
+# print(name.count('google'))
+l1 = ["apple", 5, 4.5, [1, 2, 3, 'hai'], 8+8j]
+# print(len(l1))
+'varn[index]'
+
+# print(l1[0])
+# print(l1[-5])
+# print(l1[3])
+# print(l1[-2])
+# print(l1[1])
+# print(l1[-4])
+# print(l1[0][0])
+# print(l1[-5][-5])
+print(l1[3][1])
+# print(l1[-2][-3])
+print(l1[3][3][1])
+print(l1[-2][-1][-2])
