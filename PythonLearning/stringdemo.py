226466ff821501eb9d1428e6cc8ec18d10ce19cb
krishna <saran.oct1@gmail.com>
2023-07-18 09:08:01 +0530

    :string

---
+++ PythonLearning/stringdemo.py
@@ -0,0 +1,69 @@
+# 'Convert string to list'
+# Str_1 = input("Enter the string:")
+# list_1 = Str_1.split(' ')
+# print(list_1)
+# 'Convert any iterable to string'
+# Str_2 = list(input("Enter the string:"))
+#
+# print(Str_2)
+# print(''.join(Str_2))
+# 'Convert Lower to Upper'
+# Str_3 = input("Enter the string:")
+# print(Str_3.lower())
+# # 'Convert Upper to Lower'
+# Str_4 = input("Enter the string:")
+# print(Str_4.upper())
+# 'Convert Upper to Lower and Lower to Upper'
+# Str_5 = input("Enter the string:")
+# print(Str_5.swapcase())
+# 'Check start with is substring'
+# Str_6 = input("Enter the string:")
+# Str_7 = input("Enter SubString:")
+# print(Str_6.startswith(Str_7, 2, 5))
+# 'Check End with is substring'
+# Str_8 = input("Enter the string:")
+# Str_9 = input("Enter SubString:")
+# print(Str_8.endswith(Str_9, 3, 5))
+# 'get index of substring'
+# Str_10 = input("Enter the string:")
+# Str_11 = input("Enter SubString:")
+# print(Str_10.index(Str_11, 0, 5))
+# 'get count of substring'
+# Str_12 = input("Enter the string:")
+# Str_13 = input("Enter SubString:")
+# print(Str_12.count(Str_13))
+# 'replace substring'
+# Str_14 = input("Enter the string:")
+# Str_15 = input("Enter Old SubString Need to replace:")
+# Str_16 = input("Enter New SubString:")
+# print(Str_14.replace(Str_15, Str_16))
+# 'to remove substring'
+# Str_17 = input("Enter the string:")
+# Str_18 = input("Enter SubString:")
+# print(Str_17.strip(Str_18))
+# # 'to check string is lower or not'
+# Str_19 = input("Enter the string:")
+# print(Str_19.islower())
+# 'to check string is upper or not'
+# Str_20 = input("Enter the string:")
+# print(Str_20.isupper())
+# 'to get specific character from string'
+# Str_21 = input("Enter the string:")
+# Str_22 = int(input("Enter the starting index:"))
+# Str_23 = int(input("Enter the ending index:"))
+# print(Str_21[Str_22:Str_23])
+# 'to get alternate character from string'
+# Str_24 = input("Enter the string:")
+# Str_25 = Str_24[::2]
+# print(Str_25)
+# 'combine two strings'
+# Str_26 = input("Enter the string1:\n")
+# Str_27 = input("Enter the string2:")
+# print(''.join(Str_26 + Str_27))
+# 'Reverse the strings'
+Str_28 = input("Enter the string1:")
+Str_29 = Str_28[::-1]
+print(Str_29)
+
+
+
